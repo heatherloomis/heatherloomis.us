dd1e6dafaf621481980adda4879da1a99dbf5b28
Heather Loomis <dorkyturtle@gmail.com>
2021-12-19 07:52:44 -0600

    Made each gallery use metadata in the image filename.

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
----
----
-{% assign gallery_images = site.static_files | where: "gallery", true %}
-const gallery_images = [{% for image in gallery_images %}
-	"{{ image.path }}",{% endfor %}
-].sort((a, b) => 0.5 - Math.random());
+/*
+{% assign images = site.static_files | where: "gallery", true %}
+const gallery_images = [
+{% for image in images %}{% assign parts = image.basename | split: "-" %}{% assign tags = parts[2] | split: "," %}{% if parts[1] == "V" %}"{{ image.path }}",
+{% endif %}{% endfor %}];
+*/
 
 
 window.addEventListener('load', function(e) {
 
 	let carousel_element = document.getElementById('carousel');
-	if (carousel_element) {
-		for (let i=0;i<gallery_images.length;i++) {
-			let img = document.createElement("img");
-			img.classList.add("tns-lazy-img");
-			img.setAttribute("data-src", gallery_images[i]);
-			
-			let div = document.createElement("div");
-			div.appendChild(img);
-	
-			carousel_element.appendChild(div);
-		}
-	
+	if (carousel_element) {	
 		let slider = tns({
 			container: carousel_element,
-			items: 18,
-			fixedWidth: 400,
+			//items: 18,
+			fixedWidth: 320,
 			slideBy: "page",
 			center: true,
 			controls: false,
